1f41c4695d7a9f760f7cd7071cf61f037badfbcb
Mood Vikram <vikramiitian28@gmail.com>
2025-02-04 18:53:53 +0530

    added convergence test

--- mysqrt.py
+++ mysqrt.py
@@ -1,9 +1,13 @@
-x=2.0
+x=10000.0
 s=1.0
-
-for i in range(10):
+kmax=100
+tol=1.0e-14
+for i in range(kmax):
+	s0=s
 	print(f"At interation{i} the value of s={s}")
 	s=0.5*(s+x/s)
+	if(abs(s-s0)/x<tol):
+		break
 
 print(f"finally, the value of s={s}")
 
