932dbcbcf99f416a5c475b2287a706e1bc30260b
Mood Vikram <vikramiitian28@gmail.com>
2025-02-04 19:01:58 +0530

    converted mysqrt code to a function

--- mysqrt.py
+++ mysqrt.py
@@ -1,15 +1,17 @@
-x=10000.0
-s=1.0
-kmax=100
-tol=1.0e-14
-for i in range(kmax):
-	s0=s
-	print(f"At interation{i} the value of s={s}")
-	s=0.5*(s+x/s)
-	if(abs(s-s0)/x<tol):
-		break
+def sqrt2(x):
+	s=1.0
+	kmax=100
+	tol=1.0e-14
+	for i in range(kmax):
+		s0=s
+		print(f"At interation{i} the value of s={s:20.15f}")
+		s=0.5*(s+x/s)
+		if(abs(s-s0)/x<tol):
+			break
 
-print(f"finally, the value of s={s}")
+	print(f"finally, the value of s={s:20.15f}")
+
+sqrt2(10000.0)
 
 
 
