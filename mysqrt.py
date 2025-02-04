05e29b2d9a1a675e072b9abd8f165a079f7525c4
Mood Vikram <vikramiitian28@gmail.com>
2025-02-04 22:00:08 +0530

    added debug

--- mysqrt.py
+++ mysqrt.py
@@ -1,17 +1,18 @@
-def sqrt2(x):
+def sqrt2(x,debug=False):
 	s=1.0
 	kmax=100
 	tol=1.0e-14
 	for i in range(kmax):
 		s0=s
-		print(f"At interation{i} the value of s={s:20.15f}")
+		if(debug):
+			print(f"At interation{i} the value of s={s:20.15f}")
 		s=0.5*(s+x/s)
 		if(abs(s-s0)/x<tol):
 			break
+	if(debug):
+		print(f"finally, the value of s={s:20.15f}")
+	return s
 
-	print(f"finally, the value of s={s:20.15f}")
-
-sqrt2(10000.0)
 
 
 
